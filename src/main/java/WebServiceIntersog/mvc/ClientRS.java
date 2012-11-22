36213408c04480ee17d4e296ed3f9ebde28cda7c
berrouz <berrouz@gmail.com>
2012-11-22 15:50:26 +0100

    client doesn't parse JSON response

--- src/main/java/WebServiceIntersog/mvc/ClientRS.java
+++ src/main/java/WebServiceIntersog/mvc/ClientRS.java
@@ -1,11 +1,8 @@
 package WebServiceIntersog.mvc;
 
-import WebServiceIntersog.domain.Distance;
 import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientRequest;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.core.impl.provider.entity.FormProvider;
 
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
@@ -15,7 +12,7 @@ public class ClientRS {
 
         Client client = Client.create();
         WebResource webResource = client.resource("http://localhost:8090/webservice/rest/json/distances/put");
-        String input  = "{\"cityA\":\"Moscow\",\"cityB\":\"Kiev\",\"distance\":\"1010\"}";
+        String input  = "{\"cityA\":\"Moscow\",\"cityB\":\"Paris\",\"distance\":\"2500\"}";
         ClientResponse response = webResource.type("application/json").post(ClientResponse.class, input);
 
         System.out.println("Output from Server .... \n");
@@ -23,13 +20,9 @@ public class ClientRS {
 
         WebResource webResource1 = client.resource("http://localhost:8090/webservice/rest/json/distances/get");
         String input1 = "{\"cityA\":\"Moscow\",\"cityB\":\"Kiev\",\"distance\": null}";
-        //WebResource.Builder builder = webResource1.accept(MediaType.APPLICATION_JSON);
-        //ClientResponse response2 = builder.type("appliation/json").post(ClientResponse.class, input1);
-        //Distance distance = response2.getEntity(Distance.class);
-        ClientResponse response1 = webResource1
+        ClientResponse response1 = webResource1.accept(MediaType.APPLICATION_JSON_TYPE)
                 .type("application/json").post(ClientResponse.class, input1);
-        //Distance  distance =  response1.getEntity(Distance.class);
-        System.out.println(response1.getLength());
+        System.out.println(response1);
 
 
     }
